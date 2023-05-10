245986022340251bd6cdc05f096a55b8691ed889
albertcmiller1 <albertcmiller1@gmail.com>
2023-05-10 17:28:09 -0400

    fixed bug and added new line to study.sh file

--- study.sh
+++ study.sh
@@ -1,10 +1,19 @@
+#dir=manouvers
+dir=pattern_work
+
 # find the number of files in the manouvers directory 
-numfiles=`ls -1 manouvers | wc -l`
+numfiles=`ls -1 $dir | wc -l`
 
 # find a random int between 1 and the number of files in the manouvers directory 
 randnum=$((1 + $RANDOM % $numfiles))
 
 # use sed and the random number to grab the random file 
-file=$(ls -1 manouvers | sed -n "$randnum"p)
+file=$(ls -1 $dir | sed -n "$randnum"p)
 
-cat manouvers/$file 
+echo "$dir ---> $file"
+echo 
+echo 
+echo 
+
+
+cat $dir/$file 
