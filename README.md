81384a527a843052ae619ce17452f8d2d9ace72d
albertcmiller1 <albertcmiller1@gmail.com>
2023-05-10 17:28:09 -0400

    cleaned up

---
+++ README.md
@@ -0,0 +1,11 @@
+Use this repo to study your pattern work, manouvers, and other (coming soon) single engine land private pilot material. 
+
+to get started just choose which directory you would like to stduy from and run 
+`sh study.sh maneuvers` 
+or 
+`sh study.sh pattern_work`
+
+
+example: 
+
+![](example.png)
--- study.sh
+++ study.sh
@@ -1,3 +1,5 @@
+dir=$1
+
 #dir=manouvers
 dir=pattern_work
 
@@ -10,12 +12,9 @@ randnum=$((1 + $RANDOM % $numfiles))
 # use sed and the random number to grab the random file 
 file=$(ls -1 $dir | sed -n "$randnum"p)
 
-echo "$dir ---> $file"
+echo "$dir ---> $file" && sleep 3
 echo 
 echo 
 echo 
 
 cat $dir/$file 
-
-# test test 
-
